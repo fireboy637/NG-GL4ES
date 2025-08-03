f9ba82e829dae6262b329195ebe11d96066c753c
fireboy637 <145782402+fireboy637@users.noreply.github.com>
2025-08-03 15:10:31 +0800

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -5,7 +5,7 @@ cmake_build () {
   ANDROID_ABI=$1
   mkdir -p build
   cd build
-  cmake $GITHUB_WORKSPACE/src/main/cpp -DANDROID_PLATFORM=24 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DANDROID_ARM_MODE=arm -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake
+  cmake $GITHUB_WORKSPACE -DANDROID_PLATFORM=24 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DANDROID_ARM_MODE=arm -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake
   cmake --build . --config Release --parallel 4
   $ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip $GITHUB_WORKSPACE/build/libng_gl4es.so
 }
